a00fc21cc06cf690a1f7dbf1e84c658e6de80efe
sin10259 <sin10259@sheridancollege.ca>
2020-06-02 19:06:08 -0400

    First commit

---
+++ src/week2tuesday/Week2Tuesday.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week2tuesday;
+
+/**
+ *
+ * @author amandeep
+ */
+public class Week2Tuesday {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("learn the github");
+    }
+    
+}
